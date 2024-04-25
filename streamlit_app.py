1b6e66f5e76d6a166297e10f4b339bf55e978e7e
mokhairy <m_khairy@fanshaweonline.ca>
2024-04-25 19:37:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ icon("🏎️")
 st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
 
 client = Groq(
-    api_key=st.secrets["GROQ_API_KEY"],
+    api_key=st.secrets["api_key"],
 )
 
 # Initialize chat history and selected model
